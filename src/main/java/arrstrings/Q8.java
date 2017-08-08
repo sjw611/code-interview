d4f24e5cf9f5b554fe7f9f353cf8e0c6f019a030
Jingwei Song <sjw611@outlook.com>
2017-08-08 21:02:55 +0100

    merge code written in the company for array string

---
+++ src/main/java/arrstrings/Q8.java
@@ -0,0 +1,31 @@
+package arrstrings;
+
+import java.util.Objects;
+
+public class Q8 {
+
+    public void zeroMatrix(int[][] matrix) {
+        Objects.requireNonNull(matrix);
+
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] columns = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    columns[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (rows[i] || columns[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+}
